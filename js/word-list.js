34c5134251cfb82c87355453445f2640957dad9a
Cholicer <58261264+Cholicer@users.noreply.github.com>
2023-08-31 23:52:21 +0600

    Add files via upload

---
+++ js/word-list.js
@@ -0,0 +1,22 @@
+const wordList = [
+   {
+      word: "science",
+      hint: "The systematic study of the structure and behavior of the physical shit."
+   },
+   {
+      word: "bicycle",
+      hint: "A human-powered vehicle with two wheels."
+   },
+   {
+      word: "sunset",
+      hint: "The daily disappearance of the sun below the horizon."
+   },
+   {
+      word: "coffee",
+      hint: "A popular caffeinated beverage made from roasted coffee beans."
+   },
+   {
+      word: "dance",
+      hint: "A rhythmic movement of the body often performed to music."
+   }
+];
